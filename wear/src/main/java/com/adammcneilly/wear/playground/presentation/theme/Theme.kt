979995e3b006b403fcb98ae545a311a434872114
Adam McNeilly <amcneilly331@gmail.com>
2025-03-15 16:41:32 -0400

    Fixing detekt issues.

--- wear/src/main/java/com/adammcneilly/wear/playground/presentation/theme/Theme.kt
+++ wear/src/main/java/com/adammcneilly/wear/playground/presentation/theme/Theme.kt
@@ -4,14 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.wear.compose.material.MaterialTheme
 
 @Composable
-fun WearPlaygroundTheme(
-    content: @Composable () -> Unit
-) {
+fun WearPlaygroundTheme(content: @Composable () -> Unit) {
     /**
      * Empty theme to customize for your app.
      * See: https://developer.android.com/jetpack/compose/designsystems/custom
      */
     MaterialTheme(
-        content = content
+        content = content,
     )
 }
--- wear/src/main/java/com/adammcneilly/wear/playground/tile/MainTileService.kt
+++ wear/src/main/java/com/adammcneilly/wear/playground/tile/MainTileService.kt
@@ -8,20 +8,17 @@ import androidx.wear.protolayout.LayoutElementBuilders
 import androidx.wear.protolayout.ModifiersBuilders
 import androidx.wear.protolayout.ResourceBuilders
 import androidx.wear.protolayout.TimelineBuilders
-import androidx.wear.protolayout.material.Button
 import androidx.wear.protolayout.material.CircularProgressIndicator
 import androidx.wear.protolayout.material.Colors
 import androidx.wear.protolayout.material.CompactChip
 import androidx.wear.protolayout.material.Text
 import androidx.wear.protolayout.material.Typography
 import androidx.wear.protolayout.material.layouts.EdgeContentLayout
-import androidx.wear.protolayout.material.layouts.PrimaryLayout
 import androidx.wear.tiles.RequestBuilders
 import androidx.wear.tiles.TileBuilders
 import androidx.wear.tiles.tooling.preview.Preview
 import androidx.wear.tiles.tooling.preview.TilePreviewData
 import androidx.wear.tooling.preview.devices.WearDevices
-import com.adammcneilly.wear.playground.presentation.MainActivity
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.android.horologist.tiles.SuspendingTileService
 
@@ -32,10 +29,7 @@ private const val RESOURCES_VERSION = "0"
  */
 @OptIn(ExperimentalHorologistApi::class)
 class MainTileService : SuspendingTileService() {
-
-    override suspend fun resourcesRequest(
-        requestParams: RequestBuilders.ResourcesRequest,
-    ) = resources(requestParams)
+    override suspend fun resourcesRequest(requestParams: RequestBuilders.ResourcesRequest) = resources()
 
     override suspend fun tileRequest(requestParams: RequestBuilders.TileRequest): TileBuilders.Tile {
         println("ADAMLOG - TILE REQUEST: $requestParams")
@@ -44,88 +38,91 @@ class MainTileService : SuspendingTileService() {
     }
 }
 
-private fun resources(
-    requestParams: RequestBuilders.ResourcesRequest,
-): ResourceBuilders.Resources {
-    return ResourceBuilders.Resources.Builder()
+private fun resources(): ResourceBuilders.Resources =
+    ResourceBuilders.Resources
+        .Builder()
         .setVersion(RESOURCES_VERSION)
         .build()
-}
 
 private fun tile(
     requestParams: RequestBuilders.TileRequest,
     context: Context,
 ): TileBuilders.Tile {
-    val singleTileTimeline = TimelineBuilders.Timeline.Builder()
+    val singleTileTimeline = TimelineBuilders.Timeline
+        .Builder()
         .addTimelineEntry(
-            TimelineBuilders.TimelineEntry.Builder()
+            TimelineBuilders.TimelineEntry
+                .Builder()
                 .setLayout(
-                    LayoutElementBuilders.Layout.Builder()
+                    LayoutElementBuilders.Layout
+                        .Builder()
                         .setRoot(tileLayout(requestParams, context))
                         .build(),
-                )
-                .build(),
-        )
-        .build()
+                ).build(),
+        ).build()
 
-    return TileBuilders.Tile.Builder()
+    return TileBuilders.Tile
+        .Builder()
         .setResourcesVersion(RESOURCES_VERSION)
         .setTileTimeline(singleTileTimeline)
         .build()
 }
 
+@Suppress("MagicNumber")
 private fun tileLayout(
     requestParams: RequestBuilders.TileRequest,
     context: Context,
-): LayoutElementBuilders.LayoutElement {
-    return EdgeContentLayout.Builder(requestParams.deviceConfiguration)
+): LayoutElementBuilders.LayoutElement =
+    EdgeContentLayout
+        .Builder(requestParams.deviceConfiguration)
         .setResponsiveContentInsetEnabled(true)
         .setEdgeContent(
-            CircularProgressIndicator.Builder()
+            CircularProgressIndicator
+                .Builder()
                 .setProgress(0.75F)
                 .build(),
-        )
-        .setContent(
-            LayoutElementBuilders.Column.Builder()
+        ).setContent(
+            LayoutElementBuilders.Column
+                .Builder()
                 .addContent(
-                    Text.Builder(context, "Upcoming Events")
+                    Text
+                        .Builder(context, "Upcoming Events")
                         .setColor(argb(Colors.DEFAULT.onSurface))
                         .setTypography(Typography.TYPOGRAPHY_BODY1)
                         .build(),
-                )
-                .addContent(
-                    CompactChip.Builder(
-                        context,
-                        "Click Me",
-                        ModifiersBuilders.Clickable
-                            .Builder()
-                            .setId("ActivityLaunch")
-                            .setOnClick(
-                                ActionBuilders.LaunchAction.Builder()
-                                    .setAndroidActivity(
-                                        ActionBuilders.AndroidActivity.Builder()
-                                            .setPackageName("com.adammcneilly.wear.playground")
-                                            .setClassName("com.adammcneilly.wear.playground.presentation.MainActivity")
-                                            .addKeyToExtraMapping(
-                                                "TILE_BUTTON",
-                                                ActionBuilders.stringExtra("1234"),
-                                            )
-                                            .build()
-                                    )
-                                    .build()
-                            )
-                            .build(),
-                        DeviceParametersBuilders.DeviceParameters
-                            .Builder()
-                            .build(),
-                    ).build()
-                )
-                .build(),
+                ).addContent(
+                    CompactChip
+                        .Builder(
+                            context,
+                            "Click Me",
+                            ModifiersBuilders.Clickable
+                                .Builder()
+                                .setId("ActivityLaunch")
+                                .setOnClick(
+                                    ActionBuilders.LaunchAction
+                                        .Builder()
+                                        .setAndroidActivity(
+                                            ActionBuilders.AndroidActivity
+                                                .Builder()
+                                                .setPackageName("com.adammcneilly.wear.playground")
+                                                .setClassName(
+                                                    "com.adammcneilly.wear.playground.presentation.MainActivity",
+                                                ).addKeyToExtraMapping(
+                                                    "TILE_BUTTON",
+                                                    ActionBuilders.stringExtra("1234"),
+                                                ).build(),
+                                        ).build(),
+                                ).build(),
+                            DeviceParametersBuilders.DeviceParameters
+                                .Builder()
+                                .build(),
+                        ).build(),
+                ).build(),
         ).build()
-}
 
 @Preview(device = WearDevices.SMALL_ROUND)
 @Preview(device = WearDevices.LARGE_ROUND)
-fun tilePreview(context: Context) = TilePreviewData(::resources) {
-    tile(it, context)
-}
+fun tilePreview(context: Context) =
+    TilePreviewData(::resources) {
+        tile(it, context)
+    }
